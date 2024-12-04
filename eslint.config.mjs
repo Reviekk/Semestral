5db195d4713afc7d170199350de35d7ab0299153
Reviekk <juancmol.r@gmail.com>
2024-12-04 16:41:45 -0500

    Primer Commit

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  pluginJs.configs.recommended, // Configuración recomendada
+  {
+    rules: {
+      'semi': ['error', 'always'], // Exige punto y coma
+      'no-var': 'error', // Prohíbe el uso de `var`
+      'quotes': ['error', 'single'], // Exige comillas simples
+      'indent': ['error', 2], // Indentación de 2 espacios
+      'no-unused-vars': 'error', // Detecta variables no usadas
+    },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+];
